45b6c621a04b7e9f6a6315cae230b41387ad3222
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-03-29 14:23:17 +0200

    refactor code to get resoruces from k8s

--- main.go
+++ main.go
@@ -9,42 +9,40 @@ import (
 
 func main() {
 
-	dateCmd := exec.Command("date")
-	dateOut, err := dateCmd.Output()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("> date")
-	fmt.Println(string(dateOut))
+	// dateCmd := exec.Command("date")
+	// dateOut, err := dateCmd.Output()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("> date")
+	// fmt.Println(string(dateOut))
 
-	fmt.Println("pity")
-	fmt.Printf("%q\n", strings.Split("a,b", ","))
-
-	exec.Command("sh", "-c", "echo 1 > /tmp/1.txt")
-	//fmt.Printf("%s\n\n", outtt.Run())
+	k8sFilterKey := "-o"
+	k8sFilterExpression := "jsonpath='{range .items[*]} {.metadata.name} {\" \"} {.status.startTime}  {end}'"
+	getMoonPods := "kubectl get pod -n moon"
 
 	wg := new(sync.WaitGroup)
-	wg.Add(3)
+	wg.Add(1)
 
-	x := []string{"ls#-la"}
-	go exeCmd(x[0], wg)
+	go exeCmd(getMoonPods, k8sFilterKey, k8sFilterExpression, wg)
 
 	wg.Wait()
 }
 
-func exeCmd(cmd string, wg *sync.WaitGroup) {
+func exeCmd(cmd string, k8sFilterKey string, k8sFilterExpression string, wg *sync.WaitGroup) {
 	fmt.Println("given string is ", cmd)
 	// splitting entryPoint => main command, shParts => rest of the command
-	shParts := strings.Split(cmd, "#")
+	shParts := strings.Split(cmd, " ")
 	entryPoint := shParts[0]
-	//fmt.Println(entryPoint)
 	shParts = shParts[1:len(shParts)]
-	//fmt.Println(shParts)
+	shParts = append(shParts, k8sFilterKey, k8sFilterExpression)
+	fmt.Println(shParts)
 
-	out, _ := exec.Command(entryPoint, shParts...).Output()
-	// if err != nil {
-	// 	fmt.Printf("%s", err)
-	// }
+	out, err := exec.Command(entryPoint, shParts...).Output()
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("%s", out)
 	wg.Done() // Need to signal to waitgroup that this goroutine is done
 }
