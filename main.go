608cd9657bf1beb1055dbf0bd6cb4224f220a387
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-03-29 17:49:39 +0200

    create serated fun for string formating

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"reflect"
 	"strings"
 	"sync"
 )
@@ -18,7 +19,7 @@ func main() {
 	// fmt.Println(string(dateOut))
 
 	k8sFilterKey := "-o"
-	k8sFilterExpression := "jsonpath='{range .items[*]} {.metadata.name} {\" \"} {.status.startTime}  {end}'"
+	k8sFilterExpression := "jsonpath='{range .items[*]} {.metadata.name} {.status.startTime}  {end}'"
 	getMoonPods := "kubectl get pod -n moon"
 
 	wg := new(sync.WaitGroup)
@@ -43,6 +44,24 @@ func exeCmd(cmd string, k8sFilterKey string, k8sFilterExpression string, wg *syn
 		panic(err)
 	}
 
-	fmt.Printf("%s", out)
+	fmt.Printf("%s\n", out)
+
+	// filter CLI pout
+	filterK8sOutput(out)
 	wg.Done() // Need to signal to waitgroup that this goroutine is done
+}
+
+func filterK8sOutput(out []byte) {
+
+	fmt.Println(reflect.TypeOf(out))
+	ConvertToString := strings.Trim(string(out), "'")
+	fmt.Println(reflect.TypeOf(ConvertToString))
+	fmt.Println("OUTPUT: ", ConvertToString)
+	TrimString := strings.Trim(ConvertToString, " ")
+	fmt.Println(len(TrimString))
+	fmt.Println(ConvertToString)
+	SplitString := strings.Split(ConvertToString, "   ")
+	fmt.Println(len(SplitString))
+	fmt.Println("OLOLO: ", SplitString[0])
+
 }
